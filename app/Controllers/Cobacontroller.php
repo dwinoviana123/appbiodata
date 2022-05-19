353895f10e3182a29155906ec5b822a3dd27c662
dwinoviana123 <cantikandin96@gmail.com>
2022-05-19 09:18:05 +0700

    dwinoviana

---
+++ app/Controllers/Cobacontroller.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Cobacontroller extends Controller{
+    public function  index3()
+    {
+        return view('coba2');
+    }
+}
